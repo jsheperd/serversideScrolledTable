fece2df476d422a5c6245c2692960b398e2e1127
jsheperd <juhasz.janos.flutemaker@gmail.com>
2015-12-14 22:59:42 +0100

    Adding horizontal scrollbar

---
+++ app.js
@@ -0,0 +1,51 @@
+var app = angular.module('myApp', ['ngResource']);
+
+
+app.factory('Customer', function ($resource) {
+  var odataUrl = "http://services.odata.org/V4/Northwind/Northwind.svc/Customers";
+  return $resource("Customer", {}, {
+    'query': {
+      method: 'GET',
+      params: {
+        key: "@key"
+      },
+      url: odataUrl + "(:key)"
+    },
+    'count': {
+      method: 'GET',
+      url: odataUrl + "/$count",
+      transformResponse: function (data) {
+        return {
+          value: angular.fromJson(data)
+        }
+      }
+    }
+  });
+});
+
+
+app.controller("MyController", function ($scope, Customer) {
+  $scope.skip = 0;
+  $scope.customers = [];
+  $scope.status = "";
+  $scope.count = 0;
+
+  $scope.updateQuery = function () {
+    var params = {
+      '$skip': $scope.skip,
+      '$top': 10
+    };
+    $scope.status = "Requested";
+    Customer.query(params).$promise
+      .then(function (response) {
+        $scope.customers = response.value;
+        $scope.status = "solved";
+      });
+    Customer.count().$promise
+      .then(function (response) {
+        $scope.count = response.value;
+      });
+  }
+
+  $scope.updateQuery();
+});
